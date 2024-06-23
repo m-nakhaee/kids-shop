c62fa09450a0962bcae86db19211a6b8bda01081
nakhaei <8531nakhaei@gmail.com>
2024-06-23 17:03:38 +0330

    General design of my project

---
+++ src/business/imp/ToyServiceImp.java
@@ -0,0 +1,6 @@
+package business.imp;
+
+import business.ToyService;
+
+public class ToyServiceImp implements ToyService {
+}
---
+++ src/model/dao/imp/ToyDAOImp.java
@@ -0,0 +1,6 @@
+package model.dao.imp;
+
+import model.dao.ToyDAO;
+
+public class ToyDAOImp implements ToyDAO {
+}
